3686618e00b8fa0c32fed859c325ad749497758c
Mark <111501559+Praydchicken@users.noreply.github.com>
2026-01-15 19:58:04 +0800

    feat: implement Book constructor with prototype method

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const bookLibrary = [];
+
+function Book(title, author, pages) {
+	this.id = crypto.randomUUID();
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.isRead = true;
+}
+
+Book.prototype.toggleIsRead = function () {
+	this.isRead = !this.isRead;
+	console.log(this.isRead);
+}

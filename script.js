1be7335e864a7e5c19abd3393fce97511e79c236
Mark <111501559+Praydchicken@users.noreply.github.com>
2026-01-22 13:15:14 +0800

    feat: display book card with action button

--- script.js
+++ script.js
@@ -65,4 +65,59 @@ function addBook(bookData) {
 	const book = new Book(bookData);
 	bookLibrary.push(book);
 }
+
+function createBookCard(book) {
+	const card = document.createElement('div');
+	card.className = 'book__card';
+	card.dataset.bookId = book.id;
+
+ if (book.isRead) {
+    const badge = document.createElement('div');
+    badge.className = 'book__badge';
+
+    const icon = document.createElement('span');
+    icon.className = 'material-symbols-outlined';
+    icon.textContent = 'done_all';
+
+    const text = document.createElement('p');
+    text.textContent = 'Completed';
+
+    badge.append(icon, text);
+    card.append(badge);
+  }
+
+	const bookTitle = document.createElement('h3');
+	bookTitle.className = 'book__title';
+	bookTitle.textContent = book.title;
+
+	const bookAuthor = document.createElement('p');
+	bookAuthor.className = 'book__author';
+	bookAuthor.textContent = `by ${book.author}`;
+
+	const bookPages = document.createElement('p');
+	bookPages.className = 'book__pages'
+	bookPages.textContent = `${book.pages} pages`;
+
+	const bookActions = document.createElement('div');
+	bookActions.className = 'book__actions';
+
+	const toggleReadButton = document.createElement('button');
+	toggleReadButton.classList.add('button', 'button__primary');
+	toggleReadButton.textContent = book.isRead ? 'Not Read' : 'Read';
+
+	const removeBookButton = document.createElement('button');
+	removeBookButton.classList.add('button', 'button__secondary');
+	removeBookButton.textContent = 'Remove';
+
+	bookActions.append(toggleReadButton, removeBookButton);
+	card.append(bookTitle, bookAuthor, bookPages, bookActions);
+
+	return card;
+}
+
+function updateDisplay() {
+	bookLibrary.forEach((book) => {
+		const card = createBookCard(book);
+		bookContainer.append(card);
+	});
 }
